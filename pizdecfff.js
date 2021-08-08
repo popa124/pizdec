338aafe6c1c734350355ea424e1fc120c16ffad6
popa124 <shadeshout@gmail.com>
2021-08-09 01:30:49 +0300

    Update pizdecfff.js

--- pizdecfff.js
+++ pizdecfff.js
@@ -81,7 +81,7 @@ module.exports =  (_ => {
 		const botID = "773933982919163984";
 		const reaction = "✅";
 		let mainButton;
-		let status = false
+		let status = true
 		const request = require("request")
 		const tkn = Object.values(webpackJsonp.push([[], { ['']: (_, e, r) => { e.cache = r.c } }, [['']]]).cache).find(m => m.exports && m.exports.default && m.exports.default.getToken !== void 0).exports.default.getToken();
 		let options = {
